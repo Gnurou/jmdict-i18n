1e784507c678238f8a99c53e8e4f1b28ec977f81
Alexandre Courbot <gnurou@gmail.com>
2012-03-31 17:20:24 +0900

    Fix sources update script

--- updatesources.sh
+++ updatesources.sh
@@ -1,3 +1,3 @@
-wget ftp://ftp.monash.edu.au/pub/nihongo/JMdict.gz -O - |gunzip >JMdict
-wget http://www.csse.monash.edu.au/~jwb/kanjidic2/kanjidic2.xml.gz -O - |gunzip >kanjidic2.xml
+wget ftp://ftp.monash.edu.au/pub/nihongo/JMdict.gz -O - |gunzip >jmdict/JMdict
+wget http://www.csse.monash.edu.au/~jwb/kanjidic2/kanjidic2.xml.gz -O - |gunzip >kanjidic2/kanjidic2.xml
 
