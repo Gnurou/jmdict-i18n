570fc8c6a3c1fbb0b955f6d1c615395e1cde9c87
Pierre Baumard <pbaumard@gmail.com>
2015-04-26 18:10:35 +0800

    Use rsync instead of wget

--- updatesources.sh
+++ updatesources.sh
@@ -1,3 +1,2 @@
-wget --no-verbose ftp://ftp.monash.edu.au/pub/nihongo/JMdict.gz -O - |gunzip >jmdict/JMdict
-wget --no-verbose http://www.csse.monash.edu.au/~jwb/kanjidic2/kanjidic2.xml.gz -O - |gunzip >kanjidic2/kanjidic2.xml
-
+rsync -v ftp.monash.edu.au::nihongo/JMdict jmdict/JMdict
+rsync -v ftp.monash.edu.au::nihongo/kanjidic2.xml kanjidic2/kanjidic2.xml
