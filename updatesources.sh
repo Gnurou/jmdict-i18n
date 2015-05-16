bc0c56210232d669150e49d9459135a48356942d
Pierre Baumard <pbaumard@gmail.com>
2015-05-16 18:01:28 +0800

    Automatic update with update_all

--- updatesources.sh
+++ updatesources.sh
@@ -1,2 +1,5 @@
 rsync -v ftp.monash.edu.au::nihongo/JMdict jmdict/JMdict
+# temporary fix for syntax issue
+sed -ie 's/\& /\&amp; /g' jmdict/JMdict
+
 rsync -v ftp.monash.edu.au::nihongo/kanjidic2.xml kanjidic2/kanjidic2.xml
