3a24afa774f0622b2963f1b0481de90eab45dbd9
Pierre Baumard <pbaumard@gmail.com>
2017-03-19 15:11:13 +0900

    Fixed sed

--- updatesources.sh
+++ updatesources.sh
@@ -1,6 +1,6 @@
 rsync -v ftp.monash.edu.au::nihongo/JMdict jmdict/JMdict
 
 # temporary fix for syntax issue
-sed -i '' 's/\& /\&amp; /g' jmdict/JMdict
+sed -i 's/\& /\&amp; /g' jmdict/JMdict
 
 rsync -v ftp.monash.edu.au::nihongo/kanjidic2.xml kanjidic2/kanjidic2.xml
